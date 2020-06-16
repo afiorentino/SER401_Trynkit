4fedfc285cdd1108d27cc596a5541b36cbbe28e8
root <root@ip-172-31-65-233.ec2.internal>
2020-06-16 06:41:39 +0000

    Converted file structure and contents so that it can be served up by Tomcat instead of Django

---
+++ assets/js/bluetooth.js
@@ -0,0 +1,161 @@
+
+/* constant for the service UUID */
+const service = 0x1825;
+/* constant for the TX characteristic */
+const TX_char =  '30ff6dae-fbfe-453b-8a99-9688fea23832';
+let TX_characteristic;
+/* constant for the RX characteristic */
+const RX_char = 'fbdf3e86-c18c-4e5b-aace-e7cc03257f7c';
+let RX_characteristic;
+/* variable so we can tell if the device is connected */
+var isConnected = 0;
+
+/* variable for the device */
+let bledevice;
+/* variable for the ble server */
+let bleserver;
+/* variable for the object transfer service */
+let objservice;
+
+/*
+ * bluetooth()
+ * Function for the web bluetooth functionality
+ * this opens a popup window automatically that shows devices
+ * and allows the user to select and pair.
+ *
+ * Author: Justin Bee
+ */
+function bluetooth(){
+
+     if (!navigator.bluetooth) {
+        return alert('Web Bluetooth API is not available in this browser. Please use chrome.');
+     }
+     const z = document.getElementById('debugger');
+     const y = document.getElementById('serial')
+
+     if (z.style.display ==="none"){
+         z.style.display = "block";
+     }else{
+     }
+     
+     z.innerHTML= z.innerHTML + "\n"+ ('Requesting Bluetooth Devices');
+     navigator.bluetooth.requestDevice({
+     filters:[{
+     name: 'MicroTrynkit',
+          }],
+          optionalServices: [service]
+    })
+     .then(device=>{
+     z.innerHTML = z.innerHTML + "\n"+  ("Connected to: ");
+     z.innerHTML= z.innerHTML +"\n"+  (">Name:" + device.name);
+     z.innerHTML= z.innerHTML + "\n"+ (">id:" + device.id);
+     isConnected = 1;
+     bledevice = device;
+     return device.gatt.connect();
+     })
+         .then(server=>{
+             bleserver = server;
+             return server.getPrimaryService(service);
+         })
+         .then(service => {
+             return service.getCharacteristic(RX_char)
+             .then(characteristic => {
+                 console.log(characteristic);
+                 RX_characteristic = characteristic;
+                 return service.getCharacteristic(TX_char);
+             })
+         })
+         .then(characteristic => {
+             console.log(characteristic);
+             TX_characteristic = characteristic;
+             /* add an event listener to the TX characteristic */
+             TX_characteristic.addEventListener('valueUpdate', handleValueUpdated);
+             console.log(TX_characteristic.readValue());
+         })
+         .then( value =>{
+             /* try to read from the device and print to console */
+          //   y.innerText = value.getUint8(0);
+           //  console.log(value.getUint8(0));
+         })
+     .catch(error=> {
+     z.innerHTML= z.innerHTML + "\n"+ (error);
+     });
+}
+
+/*
+ * handleValueUpdated
+ *
+ */
+function handleValueUpdated(event) {
+  //console.log(event.isReadResponse) /* Returns true if stored value comes from a read operation */
+  console.log("TX characteristic"); /* Characteristic value */
+  var value = event.target.value;
+  var data = new Uint8Array(value.buffer);
+  temp = new TextDecoder("utf-8").decode(data);
+  bleConsole(event.target.value.getUint8(0));
+  console.log(temp);
+}
+/*
+ * bleSend()
+ * This function is to send files to the ESP32 device.
+ * Is called by the send file button in the menu.
+ *
+ * Author: Justin Bee
+ */
+function bleSend() {
+    /* check if the device is connected if true send file*/
+    let encoder = new TextEncoder('utf-8');
+
+    if(isConnected){
+        /* send an erase command so that the file gets started fresh */
+        RX_characteristic.writeValue(encoder.encode("erase"));
+         for(let a = 0; a<3000; a++){
+                console.log("")
+            }
+         /* get the contents of the editor that is in use */
+        let editorContents = editor.getValue();
+        /* send the contents to the device */
+        //split the editor contents into newlines.
+        temp = editorContents.split(/\n/);
+        for(let i=0; i<temp.length; i++ )
+        {
+            //console.log(temp[i]);
+            RX_characteristic.writeValue(encoder.encode(temp[i]));
+            // delay to allow the device to handle the write
+            for(let a = 0; a<2500; a++){
+                console.log("")
+            }
+        }
+        alert("File Uploaded!")
+     }else{
+        const x = document.getElementById('console');
+        alert("MicroTrynkit device not connected. Please pair it first.");
+     }
+    //disconnect required for the device, device sees the disconnect then reboots which causes the code to run
+    bledevice.gatt.disconnect();
+}
+
+/*
+ * bleConsole()
+ * This function is to access the REPL of the ESP32 device.
+ * Called by the console button in the menu.
+ *
+ * Author: Justin Bee
+ */
+function bleConsole(value){
+    const y = document.getElementById('serial')
+    /* check if the device is connected if true send file*/
+     if(isConnected){
+         /* need to read the value of the TX_char
+          * may need some kind of loop or trigger to watch if new data comes in????
+          * not sure what I want to implement for this yet....
+          */
+       //  y.innerText = TX_characteristic.readValue(); TODO does not work
+     }else{
+         const x = document.getElementById('console');
+         x.style.display ="none";
+        alert("MicroTrynkit device not connected. Please pair it first.");
+     }
+
+}
+
---
+++ assets/js/save.js
@@ -0,0 +1,132 @@
+/**
+ * Copyright [2019] [Justin Bee, Brian Carson, Andrew Fiorentino, Abigail Ida, Vicente Ochoa]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Purpose: This file handles the saving functionality for the text editor web application
+ * SER401
+ * @file save.js
+ * @author Andrew Fiorentino 
+ * @author Abigail Ida
+ * @author Brian Carson
+ * @version January 2020
+ */
+
+
+/**
+ * saveEditorContents
+ * Saves the contents of the editor to a file of the user's choice.
+ * @author: Andrew Fiorentino
+ * @param: none
+ * @returns: none
+ **/
+
+function saveEditorContents() {
+    if (document.getElementById('code').getAttribute('name') == '.py') {
+        projectName = prompt('Please chose a name for your project: ', 'NewProject.py');
+        document.getElementById('code').setAttribute('name', projectName);
+        document.getElementById('tab0').innerText = projectName;
+    } else {
+        projectName = document.getElementById('code').getAttribute('name');
+    }
+
+
+    var element = document.createElement('a');
+    var editorContent = editor.getValue();
+    var contentArray = editorContent.split("\n");
+    var finalContent = "";
+    for(var i = 0; i < contentArray.length; i ++) {
+        finalContent = finalContent + contentArray[i] + "\n";
+    }
+    console.log(finalContent);
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(finalContent));
+    element.setAttribute('download', projectName);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+    document.body.removeChild(element);
+}
+
+/**
+ * saveTabPrompt
+ * Prompts the user to specify if they would like to save the tab they are closing while checking
+ * for duplicate file name possibilities
+ * @author: Abigail Ida, Brian Carson
+ * @param: currentTab, masterList
+ * @returns: none
+ **/
+function saveTabPrompt(currentTab, masterList){
+    if(login == null){
+        alert("Please Login first.");
+    }
+    else {
+        userResponse = confirm('Select ok to save the current tab.');
+        //get tab title and tab list
+        var tabTitle = currentTab;
+        var tList = masterList;
+        if (userResponse == true) {
+            // check for duplicate tab/file names
+            function chkDuplicates(arr) {
+                return (new Set(arr)).size !== arr.length;
+            }
+
+            if (chkDuplicates(tList)) {
+                alert("Two or more tabs are the same name! Please rename.");
+            }
+            //"save" by downloading .py file to users machine
+            else {
+                downloadEditorContents(tabTitle);
+            }
+        } else {
+            verification = alert('You have chosen not to save the contents of ' + tabTitle + ' before closing.');
+            //remove tab and corresponding contents from local storage??
+        }
+    }
+}
+
+/**
+ * clearEditorContents
+ * Clears the current contents in the editor window
+ * @author: Abigail Ida
+ * @returns: none
+ **/
+function clearEditorContents(content){
+    editor.setValue("");
+}
+
+/**
+ * downloadEditorContents
+ * saves the current contents of the editor window in a .py file by downloading it to the users machine.
+ * The name of the .py file is named after the user slected tab title listed on the corresponding tab.
+ * @author: Abigail Ida
+ * @param: tabTitle
+ * @returns: none
+ **/
+function downloadEditorContents(tabTitle) {
+    projectName = tabTitle;
+    var element = document.createElement('a');
+    var editorContent = editor.getValue();
+    var contentArray = editorContent.split("\n");
+    var finalContent = "";
+    for(var i = 0; i < contentArray.length; i ++) {
+        finalContent = finalContent + contentArray[i] + "\n";
+    }
+    console.log(finalContent);
+    //element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(document.getElementById('editor').innerText));
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(finalContent));
+    element.setAttribute('download', projectName);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
